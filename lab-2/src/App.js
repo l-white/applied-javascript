ec08a00b7207d482d57c6dda6a082f5bdec95dec
Laura <law7676@outlook.com>
2021-07-11 10:58:45 -0500

    Added Nav, Content, and Footer components

--- lab-2/src/App.js
+++ lab-2/src/App.js
@@ -1,38 +1,16 @@
+import Nav from './components/Nav';
+import Content from './components/Content';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-default">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <a className="navbar-brand" href="/#">WebSiteName</a>
-            </div>
-        <ul className="nav navbar-nav">
-          <li className="active"><a href="/#">Home</a></li>
-          <li><a href="/#">Page 1</a></li>
-          <li><a href="/#">Page 2</a></li>
-          <li><a href="/#">Page 3</a></li>
-        </ul>
-      </div>
-    </nav>
+      <Nav />
+      <Content />
+      <Footer />
   </div>
   );
 }
 
 export default App;
-/*
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
-    </ul>
-  </div>
-</nav>
-*/
---
+++ lab-2/src/components/Content.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Content = () => {
+  return (
+  <div className="container">
+    <h1>Basic Navbar Example</h1>
+    <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
+  </div>
+  );
+}
+
+export default Content;
